a270b8c7f9e115a2e1eeeeafe3b842cbc3fc4d0f
danil8665 <danilfilatov319@gmail.com>
2025-06-21 17:01:43 +0300

    deleted console.log

--- backend/db/db.js
+++ backend/db/db.js
@@ -14,12 +14,4 @@ const sequelize = new Sequelize(
   }
 );
 
-console.log({
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-});
-
 module.exports = sequelize;
